ab9b0221dd860e205da8099f3ba00b705e25b81d
Marce <73712696+m4arce@users.noreply.github.com>
2023-09-25 18:48:14 -0500

    Esto es un commit de prueba

---
+++ src/Prueba.java
@@ -0,0 +1,21 @@
+
+public class Prueba {
+	
+	private String test = "Esto es una prueba";
+
+	public Prueba(String test) {
+		super();
+		this.test = test;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+
+}
